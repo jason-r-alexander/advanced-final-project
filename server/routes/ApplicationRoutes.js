dfc42fe9500347a791c023b320e128752e563a31
jason-r-alexander <jason.alexander@fronteraconsulting.net>
2018-03-21 22:25:24 -0500

    update

--- server/routes/ApplicationRoutes.js
+++ server/routes/ApplicationRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 
 router.get("/applications", list);
-router.get("/applications/:id", show);
-router.post("/applications", create);
-router.put("/applications/:id", update);
-router.delete("/applications/:id", remove);
+//router.get("/applications/:id", show);
+//router.post("/applications", create);
+//router.put("/applications/:id", update);
+//router.delete("/applications/:id", remove);
 
 module.exports = router;
