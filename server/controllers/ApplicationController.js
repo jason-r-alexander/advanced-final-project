f6e375d90cf2eec1ec17c07469fada03e8797ce5
jason-r-alexander <jason.alexander@fronteraconsulting.net>
2018-03-21 22:32:21 -0500

    update

--- server/controllers/ApplicationController.js
+++ server/controllers/ApplicationController.js
@@ -1,41 +1,41 @@
 const ApplicationModel = require("../models/Application");
 
-exports.list = function (request, response) {
+exports.list = function (req, res) {
     //For Array
     //return response.json(comments);
 
     //For DB
     ApplicationModel.find({}).exec()
     .then(applications => {
-        return response.json(applications);
+        return res.json(applications);
     });
 }
-exports.show = function (request, response) {
+exports.show = function (req, res) {
     //For Array
     //return response.json(comments.find(comment => comment._id == request.params.id) || {});
     
     //For DB
-    ApplicationModel.findById(request.params.id).exec()
+    ApplicationModel.findById(req.params.id).exec()
     .then(application => {
-      return response.json(application);
+      return res.json(application);
     });
 }
-exports.create = function (request, response) {
+exports.create = function (req, res) {
     //For Array
     //    comments.push(request.body);
     //return response.json({});
     
     //For DB
-    const newApplication = new ApplicationModel(request.body);
+    const newApplication = new ApplicationModel(req.body);
     newApplication.save()
       .then(application => {
-        return response.json(application);
+        return res.json(application);
       });
 }
-exports.update = function (request, response) {
-    return response.json({theId: request.params.id});
+exports.update = function (req, res) {
+    return res.json({theId: req.params.id});
 }
-exports.remove = function (request, response) {
+exports.remove = function (req, res) {
     //return response.json({});
     /*
     ApplicationModel.remove(request.params.id).exec()
@@ -43,8 +43,8 @@ exports.remove = function (request, response) {
       return response.json(application);
     });
     */
-    ApplicationModel.findByIdAndRemove(request.params.id).exec()
+    ApplicationModel.findByIdAndRemove(req.params.id).exec()
     .then(application => {
-      return response.json(application);
+      return res.json(application);
     });
 }
